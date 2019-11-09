2af1db40778020f7552ccba203fe179dd85009bc
Leonid Yanchis <leonid.yanchis@gmail.com>
2019-11-09 17:58:00 -0500

    Updating some files

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-const express = require('express'), ejs = require('ejs'), url = require('url'), bodyParser = require('body-parser'), helmet = require('helmet');
-const leoRouter = require('./public/scripts/leoRouter');
+const express = require("express"),
+  ejs = require("ejs"),
+  url = require("url"),
+  bodyParser = require("body-parser"),
+  helmet = require("helmet");
+const leoRouter = require("./public/scripts/leoRouter");
 
 const app = express();
-app.use(express.static('public')); //For serving css, js, imgs..
+app.use(express.static("public")); //For serving css, js, imgs..
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
-app.use('/', leoRouter);
+app.use("/", leoRouter);
 
 //Setup express app
-app.set('views', './views');
-app.set('view engine', 'ejs');
-app.engine('html', ejs.renderFile);
+app.set("views", "./views");
+app.set("view engine", "ejs");
+app.engine("html", ejs.renderFile);
 
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
 }
 const server = app.listen(port, () => {
-    console.log(`Express running → PORT ${port}`);
-  });
+  console.log(`Express running → PORT ${port}`);
+});

1ca32937f172cc24fd4497641e4f2fd4d7e0c6b7
Leonid Yanchis <leonid.yanchis@gmail.com>
2019-11-13 02:22:23 -0500

    Updated error handling to check if images are missing from API responses

--- public/scripts/devapi.js
+++ public/scripts/devapi.js
@@ -2,7 +2,7 @@
 const fetch = require("node-fetch");
 const url = "https://dev.to/api/articles";
 const tagParam = "?tag=javascript"; //for JS articles
-const topParam = "&top=3"; //for top articles in past 7 days
+const topParam = "&top=3"; //for top articles in past 3 days
 module.exports = {
   grabArticle: async () => {
     let data, jsonData;
@@ -20,9 +20,13 @@ module.exports = {
     prom.then(result => {
       articleInfo.push(result[0].url);
       articleInfo.push(result[0].title);
-      articleInfo.push(result[0].cover_image.toString());
+
+      (result[0].cover_image == undefined) ? articleInfo.push('../images/no-img.jpeg') : articleInfo.push(result[0].cover_image.toString());
+
       articleInfo.push(result[0].positive_reactions_count);
-    })
+    }).catch((error) => {
+      console.log(JSON.stringify(error));
+    });
     return articleInfo;
   }
 

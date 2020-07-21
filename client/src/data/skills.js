2f21057d9d439fd281292fc291e66ffe38e2c431
Leonid Yanchis <leonid.yanchis@gmail.com>
2020-07-21 14:37:29 -0400

    Doing responsive things

--- client/src/data/skills.js
+++ client/src/data/skills.js
@@ -1,7 +1,7 @@
 export const skills = [
    {title: 'Javascript - ES6+', icon: 'javascript.svg', percent: '70'},
    {title: 'React - Router, Hooks', icon: 'react.svg', percent: '65'},
-   {title: 'CSS3 / HTML5 - Sass, Grid, Flexbox', icon: 'css-html.png', percent: '70'},
+   {title: 'CSS3 / HTML5 - Sass, Grid', icon: 'css-html.png', percent: '70'},
    {title: 'Node - Express, NPM', icon: 'node.png', percent: '55'},
    {title: 'Git - Github, CI / CD', icon: 'git.svg', percent: '70'},
    {title: 'Linux / Bash', icon: 'linux.png', percent: '55'},
